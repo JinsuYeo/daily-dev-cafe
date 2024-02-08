4b38c1e0537aa70ecc13344fd3da1cce7961f87e
JinsuYeo <jsyeo97@naver.com>
2024-02-08 21:27:59 +0900

    test: 공통 예외 요청에 대한 API 테스트 추가

--- dailydevcafe/src/test/java/com/jsyeo/dailydevcafe/api/AuthTest.java
+++
@@ -1,9 +0,0 @@
-package com.jsyeo.dailydevcafe.api;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@RequiredArgsConstructor
-public class AuthTest extends ApiTest {
-}
